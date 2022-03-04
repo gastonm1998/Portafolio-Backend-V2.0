64f2a92c0556c11be4613156e4173b6ddc640fb8
gastonm1998 <gastonmonasterio1998@gmail.com>
2022-03-04 18:24:51 -0300

    agregada estructura certificados python

---
+++ src/main/java/com/backend/portafoliobackend/repositories/CertPythonRepository.java
@@ -0,0 +1,9 @@
+package com.backend.portafoliobackend.repositories;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "certificadoPython")
+public class CertPythonRepository {
+}
