53db568fa6ab6228a60af767bdae92dc0bdb0d2e
gastonm1998 <gastonmonasterio1998@gmail.com>
2022-03-04 20:07:38 -0300

    verificado el CRUD de los certificados python

--- src/main/java/com/backend/portafoliobackend/controllers/CertPythonController.java
+++ src/main/java/com/backend/portafoliobackend/controllers/CertPythonController.java
@@ -56,4 +56,5 @@ public class CertPythonController {
     public CertPythonModel actualizarPython(@RequestBody CertPythonModel python){
         return this.certPythonService.actualizarPython(python);
     }
+    
 }
