6cb74955faa6c44430cd500eae8f206497549baa
gastonm1998 <gastonmonasterio1998@gmail.com>
2022-03-22 01:37:58 -0300

    creada rama para despliegue

--- src/main/java/com/backend/portafoliobackend/controllers/CertPythonController.java
+++ src/main/java/com/backend/portafoliobackend/controllers/CertPythonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Optional;
 
+
+
 @RestController
 @RequestMapping("/python")
 public class CertPythonController {
